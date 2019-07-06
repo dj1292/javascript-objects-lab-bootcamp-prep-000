e9932021e8175e64ab431127cf6c458c83a1bc4e
Dan Reeves <djreeves1292@gmail.com>
2019-07-06 02:51:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,8 +31,7 @@ function deleteFromObjectByKey(object,key) {
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   var new_obj = Object.assign({},object,{[key]:value})
-  delete new_obj.key
-  return object
+   return new_obj
 }
 
   
