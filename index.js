bbae915b9df194b756f2b13a9b9fe975372cb8b4
Dan Reeves <djreeves1292@gmail.com>
2019-07-06 02:15:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,31 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+
+function updateObjectWithKeyAndValue(object, key, value) {
+ return Object.assign({},object,{[key]:value});
+ 
+  }
+  
+  function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return object;
+  }
+  
+function deleteFromObjectByKey(object,key) {
+ var new_obj = Object.assign({},Object);
+  delete new_obj.key
+  return object 
+}
+ 
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+}
+ 
+ 
